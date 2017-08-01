92cbee536fd878abbc46c689db353aab6d7de3ad
ergot <wjose14@hotmail.com>
2017-08-01 10:26:40 +0200

    add footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <footer className="footer">
+                <div className="container">
+                    <p className="text-muted"> Copyright &copy; Company - All rights reserved </p>
+                </div>
+            </footer>
+        )
+    }
+}
+
+export default Footer;
