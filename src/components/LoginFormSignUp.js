acb3c42ca8bd186c30c70c278fc5261729af8bf5
ergot <wjose14@hotmail.com>
2017-08-01 16:56:39 +0200

    api - create new user

--- src/components/LoginFormSignUp.js
+++ src/components/LoginFormSignUp.js
@@ -1,6 +1,39 @@
 import React from 'react';
+// import Axios from "axios";
+import request from "superagent"
+
 
 class LoginFormSignUp extends React.Component {
+
+    state = {
+        name: '',
+        email: '',
+        password: '',
+        messageError: ''
+    }
+
+
+    handleSubmit = (event) =>{
+        event.preventDefault()
+        let $this = this;
+
+        request
+            .post('http://localhost:3000/api/myUsers')
+            .send(this.state)
+            .end(function(err, res) {
+
+                if(res.statusCode === 200) {
+                    console.log('--- inscription valide')
+                    $this.setState({messageError:''})
+                    return console.log(res)
+                } else {
+                    console.log('--- inscription non complété')
+                    console.log(res.body.error.message)
+                    $this.setState({messageError:res.body.error.message})
+                }
+            });
+    }
+
     render() {
         return (
             <div className="card">
@@ -8,21 +41,26 @@ class LoginFormSignUp extends React.Component {
                     <div className="center">
                         <h4 className="m-b-0"><span className="icon-text">Inscription</span></h4>
                         <p className="text-muted">Création de compte</p>
+                        <p className="text-danger">{this.state.messageError}</p>
                     </div>
-                    <form action="#" method="get">
+                    <form action="#" onSubmit={this.handleSubmit}>
                         <div className="form-group">
-                            <input type="text" className="form-control" placeholder="Full Name"></input>
+                            <input type="text" className="form-control" placeholder="Full Name"
+                                   onChange={ (event)=> this.setState({name:event.target.value})}>
+                            </input>
                         </div>
                         <div className="form-group">
-                            <input type="email" className="form-control" placeholder="Email Address"></input>
+                            <input type="text" className="form-control" placeholder="Email Address"
+                                   onChange={ (event)=> this.setState({email:event.target.value})}
+                            ></input>
                         </div>
 
                         <div className="form-group">
-                            <input type="password" className="form-control" placeholder="Password"></input>
+                            <input type="password" className="form-control" placeholder="Password"
+                                   onChange={ (event)=> this.setState({password:event.target.value})}
+                            ></input>
                         </div>
-                        <div className="form-group">
-                            <input type="password" className="form-control" placeholder="Confirm Password"></input>
-                        </div>
+
                         <button type="submit" className="btn btn-azure">Register</button>
                     </form>
                 </div>
