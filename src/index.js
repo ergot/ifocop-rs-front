75826145815e470a20fd7218509ac34158b1b033
ergot <wjose14@hotmail.com>
2017-08-02 18:10:28 +0200

    mise en place react router dom

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {  Route, Link, BrowserRouter as Router} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+//ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render((
+    <Router>
+        <Route path="/" component={App}/>
+    </Router>
+),  document.getElementById('root'));
+
 registerServiceWorker();
