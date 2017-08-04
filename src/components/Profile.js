29dc76ce6c07e543b4d932e2b6af62a7bffa0607
ergot <wjose14@hotmail.com>
2017-08-04 17:09:50 +0200

    add profile ....

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -4,6 +4,8 @@ import Footer from './Footer';
 import HomeLeftLinks from './HomeLeftLinks';
 import HomeCenterPosts from './HomeCenterPosts';
 import ProfileBigPicture from './ProfileBigPicture';
+import ProfileNavTab from './ProfileNavTab';
+import ProfileTimeLine from './ProfileTimeLine'
 
 class Profile extends React.Component {
   constructor(props) {
@@ -17,11 +19,21 @@ class Profile extends React.Component {
         <div className="container page-content">
           <div className="row">
             <div className="col-md-10 col-md-offset-1">
+              {/* profile big picture */}
               <div className="row">
                 <div className="col-md-12">
                   <ProfileBigPicture />
                 </div>
-
+              </div>
+              {/* page content */}
+              <div className="row" >
+                {/* nav tab */}
+                <ProfileNavTab />
+                  {/* Tab panes */}
+                  <div className="tab-content">
+                      {/* timeline */}
+                      <ProfileTimeLine/>
+                  </div>
               </div>
             </div>
           </div>
