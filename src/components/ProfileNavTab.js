ae92c3d1caf1a8a4fcffcd03bd07641b719f75a4
ergot <wjose14@hotmail.com>
2017-08-04 15:33:07 +0200

    profile nav tab

---
+++ src/components/ProfileNavTab.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+class ProfileNavTab extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+        <ul className="nav nav-pills nav-pills-custom-minimal custom-minimal-bottom profile-tabs">
+            <li role="presentation" className="active"><a href="#timeline" aria-controls="timeline" role="tab" data-toggle="tab" aria-expanded="true">Timeline</a></li>
+            <li role="presentation" className=""><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab" aria-expanded="false">About</a></li>
+            <li role="presentation" className=""><a href="#messages" aria-controls="messages" role="tab" data-toggle="tab" aria-expanded="false">Friends</a></li>
+            <li role="presentation" className=""><a href="#settings" aria-controls="settings" role="tab" data-toggle="tab" aria-expanded="false">Photos</a></li>
+        </ul>
+
+    );
+  }
+}
+
+export default ProfileNavTab;
