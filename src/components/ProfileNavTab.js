784d24a0ce01480357c27591b904b373dc19819c
ergot <wjose14@hotmail.com>
2017-08-04 16:33:54 +0200

    profile nav tab

--- src/components/ProfileNavTab.js
+++ src/components/ProfileNavTab.js
@@ -8,12 +8,12 @@ class ProfileNavTab extends React.Component {
 
   render() {
     return (
-        <ul className="nav nav-pills nav-pills-custom-minimal custom-minimal-bottom profile-tabs">
-            <li role="presentation" className="active"><a href="#timeline" aria-controls="timeline" role="tab" data-toggle="tab" aria-expanded="true">Timeline</a></li>
-            <li role="presentation" className=""><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab" aria-expanded="false">About</a></li>
-            <li role="presentation" className=""><a href="#messages" aria-controls="messages" role="tab" data-toggle="tab" aria-expanded="false">Friends</a></li>
-            <li role="presentation" className=""><a href="#settings" aria-controls="settings" role="tab" data-toggle="tab" aria-expanded="false">Photos</a></li>
-        </ul>
+      <ul className="nav nav-pills nav-pills-custom-minimal custom-minimal-bottom profile-tabs">
+        <li role="presentation" className="active"><a href="#timeline" aria-controls="timeline" role="tab" data-toggle="tab" aria-expanded="true">Timeline</a></li>
+        <li role="presentation" className=""><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab" aria-expanded="false">About</a></li>
+        <li role="presentation" className=""><a href="#messages" aria-controls="messages" role="tab" data-toggle="tab" aria-expanded="false">Friends</a></li>
+        <li role="presentation" className=""><a href="#settings" aria-controls="settings" role="tab" data-toggle="tab" aria-expanded="false">Photos</a></li>
+      </ul>
 
     );
   }
