1c7ca2c4c3229d0656a7c9c3ca35b8c5479361e6
ergot <wjose14@hotmail.com>
2017-08-07 13:38:17 +0200

    update le state

--- src/components/ProfileEdit.js
+++ src/components/ProfileEdit.js
@@ -1,24 +1,61 @@
 import React from 'react';
 import Header from './Header';
-import Footer from "./Footer";
+import Footer from './Footer';
+import ProfileEditInput from './ProfileEditInput';
 
 class ProfileEdit extends React.Component {
   constructor(props) {
     super(props);
+    this.submitForm = this.submitForm.bind(this);
+    this.updateStateParent = this.updateStateParent.bind(this);
   }
 
+
+  submitForm() {
+    console.log('--- submit profile edit ---');
+    console.log(this.state);
+  }
+
+  updateStateParent(label, value) {
+    const temp = {};
+    temp[label] = value;
+    this.setState(temp);
+  }
+
+
   render() {
-      return (
-          <div>
-            <Header/>
-            <div className="container page-content">
+    return (
+      <div>
+        <Header />
+        <div className="container page-content">
+          <div className="row">
+            <div className="tab-content profile-page">
+              <div className="row">
+                <div className="col-md-10 col-md-offset-1">
+                  <form className="form-horizontal">
+                    <ProfileEditInput label="Nom" updateStateParent={this.updateStateParent} />
+                    <ProfileEditInput label="Prénom" updateStateParent={this.updateStateParent} />
+                    <ProfileEditInput label="Pseudo" updateStateParent={this.updateStateParent} />
+                    <ProfileEditInput label="Mail" updateStateParent={this.updateStateParent} />
+                    <ProfileEditInput label="Coordonnées" updateStateParent={this.updateStateParent} />
+                    <ProfileEditInput label="Age" updateStateParent={this.updateStateParent} />
+                    <ProfileEditInput label="Présentation" updateStateParent={this.updateStateParent} />
+                    <ProfileEditInput label="Photo" updateStateParent={this.updateStateParent} />
+                  </form>
+                  <p className="text-center">
+                    <a href="#" className="btn btn-custom-primary" onClick={this.submitForm()}>
+                      <i className="fa fa-floppy-o" /> Save Changes</a></p>
+                </div>
 
-
+              </div>
             </div>
-            <Footer/>
           </div>
 
-      );
+        </div>
+        <Footer />
+      </div>
+
+    );
   }
 }
 
